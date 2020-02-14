31bf295fa62bd96c75c138df471eb72598b68616
Myp3ik <shdeemon@mail.ru>
2020-02-14 15:53:38 +0300

    First

---
+++ src/main/java/controllers/Controller.java
@@ -0,0 +1,12 @@
+package controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+@org.springframework.stereotype.Controller
+public class Controller {
+
+    @GetMapping("/")
+    public String main(){
+        return "index";
+    }
+}
