20c526cd84433096b3b83313afa222aa1a07d378
Myp3ik <shdeemon@mail.ru>
2020-04-20 21:44:55 +0300

    Correct

--- src/main/java/com.learn/controllers/RegController.java
+++ src/main/java/com.learn/controllers/RegController.java
@@ -14,7 +14,6 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.File;
 import java.io.IOException;
 import java.util.Collections;
-import java.util.UUID;
 
 @Controller
 public class RegController {
